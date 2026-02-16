9a80bc471800fa68026c1a606334adbd070b1d5f
user <thdjun92@naver.com>
2026-02-16 16:48:42 +0900

    Member CRUD

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -1,8 +1,12 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.MemberRequest;
+import com.example.demo.dto.MemberResponse;
 import com.example.demo.model.Member;
 import com.example.demo.repository.MemberRepository;
+import com.example.demo.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,26 +17,37 @@ import java.util.List;
 public class MemberController {
 
     private final MemberRepository memberRepository;
+    private final MemberService memberService;
 
     @PostMapping
     public Member post(@RequestBody Member member) {
         return memberRepository.save(member);
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public MemberResponse create(@RequestBody MemberRequest memberRequest) {
+        return memberService.create(memberRequest);
+    }
+
     @GetMapping
-    public List<Member> getAll() {
-        return memberRepository.findAll();
+    public List<MemberResponse> getAll() {
+        return memberService.findAll();
     }
 
     @GetMapping("/{id}")
-    public Member get(@PathVariable("id") Long id) {
-        return memberRepository.findById(id).orElseThrow();
+    public MemberResponse get(@PathVariable("id") Long id) {
+        return memberService.findById(id);
     }
 
     @PutMapping("/{id}")
-    public Member put(@PathVariable("id") Long id, @RequestBody Member member) {
-        member.setId(id);
-        return memberRepository.save(member);
+    public MemberResponse put(@PathVariable("id") Long id, @RequestBody MemberRequest memberRequest) {
+        return memberService.update(id, memberRequest);
+    }
+
+    @PatchMapping("/{id}")
+    public MemberResponse patch(@PathVariable("id") Long id, @RequestBody MemberRequest memberRequest) {
+        return memberService.patch(id, memberRequest);
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -1,10 +1,14 @@
 package com.example.demo.service;
 
+import com.example.demo.dto.MemberRequest;
 import com.example.demo.dto.MemberResponse;
+import com.example.demo.exception.NotFoundException;
 import com.example.demo.model.Member;
 import com.example.demo.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +22,68 @@ public class MemberService {
                 .email(member.getEmail())
                 .age(member.getAge())
                 .build();
+    }
+
+    @Transactional
+    public MemberResponse create(MemberRequest memberRequest) {
+        Member member = Member.builder()
+                .name(memberRequest.getName())
+                .email(memberRequest.getEmail())
+                .age(memberRequest.getAge())
+                .enabled(true)
+                .build();
+        memberRepository.save(member);
+        return mapToMemberResponse(member);
+    }
+
+    @Transactional(readOnly = true)
+    public List<MemberResponse> findAll() {
+        return memberRepository
+                .findAll()
+                .stream()
+                .map(this::mapToMemberResponse)
+                .toList();
+    }
+
+    @Transactional(readOnly = true)
+    public MemberResponse findById(Long id) {
+        Member member = memberRepository.findById(id).orElseThrow(NotFoundException::new);
+        return mapToMemberResponse(member);
+    }
+
+    @Transactional
+    public MemberResponse update(Long id, MemberRequest memberRequest) {
+        Member member = memberRepository.findById(id).orElseThrow(NotFoundException::new);
+        member.setName(memberRequest.getName());
+        member.setEmail(memberRequest.getEmail());
+        member.setAge(memberRequest.getAge());
+        //memberRepository.save(member); // 트랜젝션 어노테이션있음 안해도됨
+
+        return mapToMemberResponse(member);
+    }
+
+    @Transactional
+    public MemberResponse patch(Long id, MemberRequest memberRequest) {
+        Member member = memberRepository.findById(id).orElseThrow(NotFoundException::new);
+
+        //전달된 필드만 업데이트
+        if(memberRequest.getName() != null) {
+            member.setName(memberRequest.getName());
+        }
+        if(memberRequest.getEmail() != null) {
+            member.setEmail(memberRequest.getEmail());
+        }
+        if(memberRequest.getAge() != null) {
+            member.setAge(memberRequest.getAge());
+        }
+
+        memberRepository.save(member);
+        return mapToMemberResponse(member);
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        Member member = memberRepository.findById(id).orElseThrow(NotFoundException::new);
+        memberRepository.delete(member);
     }
 }
