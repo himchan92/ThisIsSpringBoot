cd5d45634d4899110f60d200a0e25b27bcdd14ff
user <thdjun92@naver.com>
2026-02-16 16:31:45 +0900

    init

---
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Member;
+import com.example.demo.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/members")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @PostMapping
+    public Member post(@RequestBody Member member) {
+        return memberRepository.save(member);
+    }
+
+    @GetMapping
+    public List<Member> getAll() {
+        return memberRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Member get(@PathVariable("id") Long id) {
+        return memberRepository.findById(id).orElseThrow();
+    }
+
+    @PutMapping("/{id}")
+    public Member put(@PathVariable("id") Long id, @RequestBody Member member) {
+        member.setId(id);
+        return memberRepository.save(member);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Long id) {
+        memberRepository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -0,0 +1,22 @@
+package com.example.demo.service;
+
+import com.example.demo.dto.MemberResponse;
+import com.example.demo.model.Member;
+import com.example.demo.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    private MemberResponse mapToMemberResponse(Member member) {
+        return MemberResponse.builder()
+                .id(member.getId())
+                .name(member.getName())
+                .email(member.getEmail())
+                .age(member.getAge())
+                .build();
+    }
+}
